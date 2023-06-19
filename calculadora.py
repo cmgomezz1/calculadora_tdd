7f2179f5838a3d7dfd389af620325a609e99b033
CARLOS MIGUEL GOMEZ ZAMORA <1055283@horusur.corp>
2023-06-19 16:20:06 -0600

    add first steps

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+class Calculadora:
+
+    def sumar(self, a, b):
+        return a + b
+    
+    def restar(self, a, b):
+        return a - b
+    
+    def dividir(self, a, b):
+        try:
+            return a / b
+        except Exception:
+            raise ErrorAlDividirEntreZero()
+        
+    
+
+class ErrorAlDividirEntreZero(Exception):
+    pass
---
+++ test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import unittest
+from calculadora import Calculadora, ErrorAlDividirEntreZero
+
+
+class TestCalculadora(unittest.TestCase):
+
+    def test_sumar_2_y_2_regresa_4(self):
+        calcu = Calculadora()
+        resultado = calcu.sumar(2, 2)
+
+        self.assertEqual(resultado, 4)
+
+
+    def test_sumar_3_y_5_regresa_8(self):
+        calcu = Calculadora()
+        resultado = calcu.sumar(3, 5)
+
+        self.assertEqual(resultado, 8)
+
+
+    def test_restar_8_y_2_regresa_6(self):
+        calcu = Calculadora()
+        resultado = calcu.restar(8, 2)
+
+        self.assertEqual(resultado, 6)
+
+
+    def test_restar_5_y_5_regresa_0(self):
+        calcu = Calculadora()
+        resultado = calcu.restar(5, 5)
+
+        self.assertEqual(resultado, 0)
+
+
+    def test_dividir_10_y_0_regresa_mensaje_error(self):
+        calcu = Calculadora()
+
+        with self.assertRaises(ErrorAlDividirEntreZero):
+            resultado = calcu.dividir(10, 0)
+    
+        
